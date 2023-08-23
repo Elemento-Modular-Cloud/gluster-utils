c1748883a0e61d7ff881bbfc438a9896049fb233
Gabriele Fronzé <sucre.91@hotmail.it>
2023-08-23 22:50:52 +0200

    Update create-volume.sh

--- create-volume.sh
+++ create-volume.sh
@@ -17,7 +17,7 @@ do
      echo "$i is unreachable. Aborting."
      exit 1
    fi
-   ssh $USER@$i "mkdir -p $PATH"
+   /usr/bin/ssh $USER@$i "mkdir -p $PATH"
    SERVERS_STRING="$SERVERS_STRING $i$PATH"
 done
 
