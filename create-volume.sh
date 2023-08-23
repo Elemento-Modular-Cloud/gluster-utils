8276b451f514a6227a6a75af5552d7ba20602c84
Gabriele Fronzé <sucre.91@hotmail.it>
2023-08-23 22:35:39 +0200

    Update create-volume.sh

--- create-volume.sh
+++ create-volume.sh
@@ -5,11 +5,13 @@ OPTIONS='replica 3'
 TRANSPORT='tcp'
 PATH='/mnt/raid1/gluster'
 declare -a SERVERS=("zima1.elementohq" "zima2.elementohq" "zima3.elementohq")
-
+SERVERS_STRING=""
 for i in "${SERVERS[@]}"
 do
    echo "$i"
-   # or do whatever with individual element of the array
+   SERVERS_STRING=SERVERS_STRING+"$i$PATH "
 done
 
-BASE_CMD=gluster volume create $NAME $OPTIONS transport $TRANSPORT zima1.elementohq:/mnt/raid1/gluster/datavault zima2.elementohq:/mnt/raid1/gluster/datavault zima3.elementohq:/mnt/raid1/gluster/datavault
+echo "$SERVERS_STRING"
+
+BASE_CMD="gluster volume create $NAME $OPTIONS transport $TRANSPORT zima1.elementohq:/mnt/raid1/gluster/datavault zima2.elementohq:/mnt/raid1/gluster/datavault zima3.elementohq:/mnt/raid1/gluster/datavault"
