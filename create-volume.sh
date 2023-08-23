6c71cfd95f12ea07d55f3c5252e7d5496d9aa6f4
Gabriele Fronzé <sucre.91@hotmail.it>
2023-08-23 23:12:27 +0200

    Update create-volume.sh

--- create-volume.sh
+++ create-volume.sh
@@ -6,7 +6,7 @@ echo $PATH
 NAME="$1"
 OPTIONS='replica 3'
 TRANSPORT='tcp'
-PATH='/mnt/raid1/gluster'
+TARGET_PATH='/mnt/raid1/gluster'
 HOSTNAME=$(/usr/bin/hostname)
 declare -a SERVERS=("zima1.elementohq" "zima2.elementohq" "zima3.elementohq")
 
@@ -16,7 +16,7 @@ do
    if [ $i == $HOSTNAME ]
    then
       echo "$i is localhost"
-      sudo mkdir -p "$PATH"
+      sudo mkdir -p "$TARGET_PATH"
    else
       if [ "`ping -c 1 $i`" ]
       then
@@ -25,9 +25,9 @@ do
         echo "$i is unreachable. Aborting."
         exit 1
       fi
-      ssh $USER@$i -t "sudo mkdir -p $PATH"
+      ssh $USER@$i -t "sudo mkdir -p $TARGET_PATH"
    fi
-   SERVERS_STRING="$SERVERS_STRING $i$PATH"
+   SERVERS_STRING="$SERVERS_STRING $i$TARGET_PATH"
 done
 
 GLUSTER_CMD="sudo gluster volume create $NAME $OPTIONS transport $TRANSPORT $SERVERS_STRING"
