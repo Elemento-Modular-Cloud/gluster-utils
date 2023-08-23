bf5fa374d4be7d6339d9d9291b40ee23b1f1e9cb
Gabriele Fronzé <sucre.91@hotmail.it>
2023-08-23 23:14:58 +0200

    Update create-volume.sh

--- create-volume.sh
+++ create-volume.sh
@@ -11,24 +11,33 @@ declare -a SERVERS=("zima1.elementohq" "zima2.elementohq" "zima3.elementohq")
 SERVERS_STRING=""
 for i in "${SERVERS[@]}"
 do
+   echo
+   echo "Working on $i"
    if [ $i == $HOSTNAME ]
    then
       echo "$i is localhost"
+      echo "Creating target directory"
       sudo mkdir -p "$TARGET_PATH"
    else
       if [ "`ping -c 1 $i`" ]
       then
-        echo "$i is available"
+        echo "$i is pingable"
       else
         echo "$i is unreachable. Aborting."
         exit 1
       fi
+      echo "Creating target directory"
       ssh $USER@$i -t "sudo mkdir -p $TARGET_PATH"
    fi
    SERVERS_STRING="$SERVERS_STRING $i$TARGET_PATH"
+   echo
 done
 
+echo
+echo "Creating gluster volume"
 GLUSTER_CMD="sudo gluster volume create $NAME $OPTIONS transport $TRANSPORT $SERVERS_STRING"
+echo "Command to be executed:"
+echo "   $GLUSTER_CMD"
 
 # "$GLUSTER_CMD"
 
